85163c0a33e7ba9e2dac11b709cd95ec6cc28fa8
Your Name <youremail@example.com>
2023-06-18 16:16:51 +0300

    commit message

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+beforeEach(function () {
+cats.length = 0;
+
+cats.push("Milo", "Otis", "Garfield");
+});
+function destructivelyAppendCat(name) {
+cats.push(name);
+}
+function destructivelyPrependCat(name) {
+cats.unshift(name);
+}
+function destructivelyRemoveLastCat() {
+cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+cats.shift();
+}
+function appendCat(name) {
+const newCats = [...cats, name];
+return newCats;
+}
+function prependCat(name) {
+const newCats = [name, ...cats];
+return newCats;
+}
+function removeLastCat() {
+const newCats = cats.slice(0, -1);
+return newCats;
+}
+function removeFirstCat() {
+const newCats = cats.slice(1);
+return newCats;
+}
